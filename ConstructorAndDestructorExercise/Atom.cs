94d9638c390b3961743f96e005bfed14d281613d
Cameron Graybill <cdgraybill@gmail.com>
2020-01-24 09:57:43 -0600

    Add project files.

---
+++ ConstructorAndDestructorExercise/Atom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAndDestructorExercise
+{
+    class Atom
+    {
+        public int Protons { get; set; }
+        public int Electrons { get; set; }
+    }
+
+    class DestroyAtom
+    {
+        ~DestroyAtom()
+        {
+            Console.WriteLine("Oh no");
+        }
+    }
+}
---
+++ ConstructorAndDestructorExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorAndDestructorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
